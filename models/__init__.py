b95ae3c9b8642ea5279e8a69a39791c484d8d1a0
Mohaiminus Sakib <3596793+rode093@users.noreply.github.com>
2023-07-10 12:40:36 +0200

    works

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,13 @@
 from sqlalchemy.orm import declarative_base
 from services.db import DB
-from sqlalchemy import Column, String, text, DateTime, ForeignKey
+from sqlalchemy import Column, String, text, DateTime, ForeignKey, MetaData
 from sqlalchemy.orm import relationship
 import uuid
-Base = declarative_base()
+Base = declarative_base(metadata=MetaData(schema='public'))
 
 
 class UserStatus(Base):
-    __tablename__ = 'user_statuses'
-    __table_args__ = {'schema': 'public'}  # Specify the schema name
+    __tablename__ = 'user_status'
 
     code = Column(String, primary_key=True)
     label = Column(String, nullable=False)
@@ -16,8 +15,7 @@ class UserStatus(Base):
 
 
 class User(Base):
-    __tablename__ = 'users'
-    __table_args__ = {'schema': 'public'}  # Specify the schema name
+    __tablename__ = 'user'
 
     id = Column(
         String,
@@ -30,6 +28,6 @@ class User(Base):
     password = Column(String, nullable=False)
     reset_token = Column(String, nullable=True)
     status_code = Column(String, ForeignKey(
-        "user_statuses.code"))
+        "user_status.code"))
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=True)
